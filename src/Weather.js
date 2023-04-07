b3e66ba27f2ff305e1eb401e37fa64fe0e29d731
SabinaAkhundova <sabina.akhundova@gmail.com>
2023-04-07 17:37:12 +0300

    Create WeatherIcon.js

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ setWeatherData({
     date:new Date (response.data.dt*1000),
     city:response.data.name,
     description:response.data.weather[0].description,
-    iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    icon:response.data.weather[0].icon,
     temperature:response.data.main.temp,
     humidity:response.data.main.humidity,
     wind:response.data.wind.speed,
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
     import React  from "react";
     import FormattedDate from './FormattedDate';
+    import WeatherIcon from "./WeatherIcon";
 
     export default function WeatherInfo(props) 
         {
@@ -11,9 +12,8 @@
         <FormattedDate date={props.data.date}/>
         </p>    
       <h4 className="card-title" >{props.data.city}</h4>
-      <img src={props.data.iconUrl}
-      alt={props.data.description}
-      />
+      <WeatherIcon code={props.data.icon} />
+      
       </div>
       <div className="card-info">
             <li className="list-group-item" >Now it is {props.data.description}</li>
