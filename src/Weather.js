194086e3380ac8723d4c6abb016bdd91613e38d5
SabinaAkhundova <sabina.akhundova@gmail.com>
2023-04-07 16:31:31 +0300

    Create WeatherInfo

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import FormattedDate from './FormattedDate';
 import WeatherInfo from './WeatherInfo';
 import axios from "axios";
 import "./Weather.css"
 
 export default function Weather (props){
-    
-    const[weatherData, setWeatherData]=useState({ready:false});
+const[weatherData, setWeatherData]=useState({ready:false});
+const[city, setCity]=useState (props.defaultCity);
+
     function handleResponse(response){
         console.log(response.data);
 setWeatherData({
@@ -14,71 +14,56 @@ setWeatherData({
     date:new Date (response.data.dt*1000),
     city:response.data.name,
     description:response.data.weather[0].description,
-    iconUrl:"https://openweathermap.org/img/wn/04d@2x.png",
+    iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     temperature:response.data.main.temp,
     humidity:response.data.main.humidity,
     wind:response.data.wind.speed,
     pressure:response.data.main.pressure
     
 });
-    
-    }
+}
+
+function search(){
+  const apiKey="2c6da8dad3cff7b7ea9b815b83b08d69";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+
+}
+
+function handleSudmit(event)
+{
+  event.preventDefault();
+  search();
+
+}
+
+function handleCityChange(event){
+setCity(event.target.value);
+}
+
 
     if (weatherData.ready){ 
         return(
         <div className="Weather">
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSudmit}>
       <input
         type="text"
         placeholder="Enter your city here"
         autocComplete="off"
         autoFocus="on"
+        onChange={handleCityChange}
   
       />
       <input type="submit" value="search" />
     </form>
-    <WeatherInfo/>
+    <WeatherInfo data={weatherData}/>
 
-    <form className="body-form">
-        <div className="card-header">
-          <p className="current-date">
-            <FormattedDate date={weatherData.date}/>
-            </p>    
-          <h4 className="card-title" >{weatherData.city}</h4>
-          <img src={weatherData.iconUrl}
-          alt={weatherData.description}
-          />
-          </div>
-          <div className="card-info">
-                <li className="list-group-item" >Now it is {weatherData.description}</li>
-          <li className="list-group-item">
-            <a href="#">{Math.round(weatherData.temperature)}
-            <span className="unit">°С</span>
-            </a>
-          </li>
-          <li className="list-group-item">
-            <a href="#">Fahrenheit</a>
-          </li>
-          <li className="list-group-item">{weatherData.wind} MpH</li>
-          <li className="list-group-item" >{weatherData.humidity} %</li>
-          <li className="list-group-item" >{weatherData.pressure} mmHg</li>
-          <br />
-          </div>  
-        </form> 
-          <div className="row">
-          <h5 className="forecast-title">
-            Forecast</h5>
-            <div className="forecast-text">
-              </div>
-             </div>
             
   </div>
     );
     }
     else{
-        const apiKey="2c6da8dad3cff7b7ea9b815b83b08d69";
-               let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);
+      search();
         return "Loading..."
     }
    
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,42 @@
     import React  from "react";
+    import FormattedDate from './FormattedDate';
 
-    export default function WeatherInfo() 
+    export default function WeatherInfo(props) 
         {
-        return"Weather Info";
+        return(
+            <dv className="WeatherInfo">
+    <form className="body-form">
+    <div className="card-header">
+      <p className="current-date">
+        <FormattedDate date={props.data.date}/>
+        </p>    
+      <h4 className="card-title" >{props.data.city}</h4>
+      <img src={props.data.iconUrl}
+      alt={props.data.description}
+      />
+      </div>
+      <div className="card-info">
+            <li className="list-group-item" >Now it is {props.data.description}</li>
+      <li className="list-group-item">
+        <a href="#">{Math.round(props.data.temperature)}
+        <span className="unit">°С</span>
+        </a>
+      </li>
+      <li className="list-group-item">
+        <a href="#">Fahrenheit</a>
+      </li>
+      <li className="list-group-item">{props.data.wind} MpH</li>
+      <li className="list-group-item" >{props.data.humidity} %</li>
+      <li className="list-group-item" >{props.data.pressure} mmHg</li>
+      <br />
+      </div>  
+    </form> 
+      <div className="row">
+      <h5 className="forecast-title">
+        Forecast</h5>
+        <div className="forecast-text">
+          </div>
+         </div>
+         </dv>
+        );
     }
