b5933f000df3119f17010497bd8affb75f0678f6
SabinaAkhundova <sabina.akhundova@gmail.com>
2023-04-20 19:00:14 +0300

    updated css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,24 +7,24 @@
     export default function WeatherInfo(props) 
         {
         return(
-            <div className="WeatherInfo">
-    <form className="row row-cols-2">
+    <div className="WeatherInfo">
     <div className="card-header">
       <p className="current-date">
         <FormattedDate date={props.data.date}/>
         </p>    
-      <h4 className="card-title" >{props.data.city}</h4>
-      <WeatherIcon code={props.data.icon} size={82} />
-      </div>
+      <h4 className="card-title " >{props.data.city}</h4>
+      <WeatherIcon code={props.data.icon} size={50} />
+   
       <div className="list-item">
-            <li className="list-group-item" >Now it is {props.data.description}</li>
+            <li className="list-group-item">Now it is {props.data.description}</li>
             <WeatherTemperature celsius={props.data.temperature}/>
             <li className="list-group-item">{props.data.wind} KmpH</li>
       <li className="list-group-item" >{props.data.humidity} %</li>
       <li className="list-group-item" >{props.data.pressure} mmHg</li>
       <br />
       </div>
-    </form> 
+      </div>
+    
       </div>
         );
     }
