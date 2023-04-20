026f319632302e3924c042513e3a54c6e02693e8
SabinaAkhundova <sabina.akhundova@gmail.com>
2023-04-20 20:43:29 +0300

    Update WeatherForecast
    added return to map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,7 +26,9 @@ if(loaded){
         <WeatherForecastDay data={dailyForecast}/> 
         </div>  
         );
-      }
+          }else{
+            return null;
+          }
     })}
  </div>
  </div>
